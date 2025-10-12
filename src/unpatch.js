18007ce840df9f74b31b4afc10ba94846398471b
lionel-rowe <lionel.rowe@gmail.com>
2025-10-12 17:42:25 +0800

    Convert to ES5

--- src/unpatch.js
+++ src/unpatch.js
@@ -1,119 +1,148 @@
 // @ts-check
+// deno-lint-ignore-file no-var no-this-alias
 /* @ts-self-types="./unpatch.d.ts" */
 'use strict'
 
 /** @typedef {import('./types.ts').Shared} Shared */
 /** @typedef {import('./types.ts').Version} Version */
 
-const CORE_JS_SHARED = '__core-js_shared__'
-/** @type {typeof globalThis & { [CORE_JS_SHARED]?: Shared }} */
-const global = globalThis
-const MIN_SUPPORTED_VERSION = '3.22.4'
+void (function () {
+	var CORE_JS_SHARED = /** @type {const} */ ('__core-js_shared__')
+	/** @type {typeof globalThis & { [CORE_JS_SHARED]?: Shared }} */
+	var global = globalThis
+	var MIN_SUPPORTED_VERSION = '3.22.4'
 
-let descriptor = /** @type {PropertyDescriptor} */ (Object.getOwnPropertyDescriptor(Function.prototype, 'toString'))
+	var descriptor = /** @type {PropertyDescriptor} */ (Object.getOwnPropertyDescriptor(Function.prototype, 'toString'))
 
-class TrappedArray extends Array {
-	/**
-	 * @override
-	 * @param {Version[]} items
-	 * @returns {number}
-	 */
-	push(...items) {
-		if (items[0] == null) return super.push(...items)
+	function TrappedArray() {}
 
-		const { version, mode } = items[0]
+	/** @override */
+	TrappedArray.prototype.push = function push() {
+		/** @type {Version[]} */
+		var items = Array.prototype.slice.call(arguments)
+		var self = this
+
+		if (items[0] == null) return Array.prototype.push.apply(self, items)
+
+		var version = items[0].version
+		var mode = items[0].mode
 
 		if (mode === 'global') {
 			if (semverCompare(version, MIN_SUPPORTED_VERSION) < 0) {
 				// deno-lint-ignore no-console
 				console.warn(
-					`Cannot prevent monkey patching of Function.prototype.toString for core-js version ${version} (minimum supported version is ${MIN_SUPPORTED_VERSION})`,
+					'Cannot prevent monkey patching of Function.prototype.toString for core-js version ' + version +
+						' (minimum supported version is ' + MIN_SUPPORTED_VERSION + ')',
 				)
-				return super.push(...items)
+				return Array.prototype.push.apply(self, items)
 			}
 
 			restoreOrSetupTrap()
 		}
 
-		return super.push(...items)
-	}
-}
-
-if (Array.isArray(global[CORE_JS_SHARED]?.versions) && typeof global[CORE_JS_SHARED]?.inspectSource === 'function') {
-	// deno-lint-ignore no-console
-	console.warn('Already patched; re-patching with `inspectSource`')
-	const { inspectSource } = global[CORE_JS_SHARED]
-	descriptor = {
-		...descriptor,
-		value: function toString() {
-			return inspectSource(this)
-		},
+		return Array.prototype.push.apply(self, items)
 	}
 
-	restoreDescriptor()
-	Object.setPrototypeOf(global[CORE_JS_SHARED].versions, TrappedArray.prototype)
-} else {
-	trapAccessorsOnce(global, CORE_JS_SHARED, /** @type {Shared} */ ({ versions: new TrappedArray() }))
-}
+	Object.setPrototypeOf(TrappedArray.prototype, Array.prototype)
+	Object.setPrototypeOf(TrappedArray, Array)
 
-/**
- * @param {unknown} obj
- * @param {string} key
- * @param {unknown} value
- * @param {() => void} [cb]
- */
-function trapAccessorsOnce(obj, key, value, cb) {
-	const descriptor = Object.getOwnPropertyDescriptor(obj, key)
+	var shared = global[CORE_JS_SHARED]
+	/** @type {((x: unknown) => string)} */
+	var inspectSource
+	if (shared != null && Array.isArray(shared.versions) && typeof shared.inspectSource === 'function') {
+		// deno-lint-ignore no-console
+		console.warn('Already patched; re-patching with `inspectSource`')
+		inspectSource = shared.inspectSource
+		descriptor = merge(
+			descriptor,
+			{
+				value: function toString() {
+					return inspectSource(this)
+				},
+			},
+		)
 
-	Object.defineProperty(obj, key, {
-		get() {
-			Object.defineProperty(obj, key, { ...descriptor, value })
-			cb?.()
-			return value
-		},
-		set(newValue) {
-			cb?.()
-			Object.defineProperty(obj, key, { ...descriptor, value: value ?? newValue })
-		},
-		configurable: true,
-	})
-}
-
-function restoreOrSetupTrap() {
-	if (isCurrentlyPatched()) restoreDescriptor()
-	else setupTrap()
-}
-
-function setupTrap() {
-	Object.defineProperty(Function.prototype, 'toString', {
-		get() {
-			return descriptor.value
-		},
-		set() {
-			restoreDescriptor()
-		},
-		configurable: true,
-	})
-}
-
-function restoreDescriptor() {
-	Object.defineProperty(Function.prototype, 'toString', descriptor)
-}
-
-function isCurrentlyPatched() {
-	return Function.prototype.toString !== descriptor.value
-}
-
-/**
- * @param {string} a
- * @param {string} b
- */
-function semverCompare(a, b) {
-	const pa = a.split('.').map(Number)
-	const pb = b.split('.').map(Number)
-	for (let i = 0; i < 3; i++) {
-		const diff = (pa[i] ?? 0) - (pb[i] ?? 0)
-		if (diff) return diff
+		restoreDescriptor()
+		Object.setPrototypeOf(shared.versions, TrappedArray.prototype)
+	} else {
+		trapAccessorsOnce(global, CORE_JS_SHARED, /** @type {Shared} */ ({ versions: new TrappedArray() }))
 	}
-	return 0
-}
+
+	/**
+	 * @param {unknown} obj
+	 * @param {string} key
+	 * @param {unknown} value
+	 * @param {() => void} [cb]
+	 */
+	function trapAccessorsOnce(obj, key, value, cb) {
+		var descriptor = Object.getOwnPropertyDescriptor(obj, key)
+
+		Object.defineProperty(obj, key, {
+			get: function () {
+				Object.defineProperty(obj, key, merge(descriptor, { value: value }))
+				if (cb != null) cb()
+				return value
+			},
+			set: function (newValue) {
+				if (cb != null) cb()
+				Object.defineProperty(obj, key, merge(descriptor, { value: value || newValue }))
+			},
+			configurable: true,
+		})
+	}
+
+	function restoreOrSetupTrap() {
+		if (isCurrentlyPatched()) restoreDescriptor()
+		else setupTrap()
+	}
+
+	function setupTrap() {
+		Object.defineProperty(Function.prototype, 'toString', {
+			get: function () {
+				return descriptor.value
+			},
+			set: function () {
+				restoreDescriptor()
+			},
+			configurable: true,
+		})
+	}
+
+	function restoreDescriptor() {
+		Object.defineProperty(Function.prototype, 'toString', descriptor)
+	}
+
+	function isCurrentlyPatched() {
+		return Function.prototype.toString !== descriptor.value
+	}
+
+	/**
+	 * @param {string} a
+	 * @param {string} b
+	 */
+	function semverCompare(a, b) {
+		var pa = a.split('.').map(Number)
+		var pb = b.split('.').map(Number)
+		var i, diff
+		for (i = 0; i < 3; i++) {
+			diff = (pa[i] || 0) - (pb[i] || 0)
+			if (diff) return diff > 0 ? 1 : -1
+		}
+		return 0
+	}
+
+	function merge() {
+		/** @type {Record<string, unknown>} */
+		var out = {}
+		var obj, i, keys, j
+		for (i = 0; i < arguments.length; i++) {
+			if (arguments[i] == null) continue
+			obj = arguments[i]
+			keys = Object.keys(obj)
+			for (j = 0; j < keys.length; j++) {
+				out[keys[j]] = obj[keys[j]]
+			}
+		}
+		return out
+	}
+})()
